0ed082252e689ddb7518f43b29027d9486cc861a
AnaMCarrenoA <128761853+AnaMCarrenoA@users.noreply.github.com>
2023-09-06 23:43:57 -0400

    Ana hack_4 y Hack_8

--- hack_4/hack_4_while.js
+++ hack_4/hack_4_while.js
@@ -5,8 +5,16 @@
  * 
  * output => [1,3,5,7]
  */
+let array = [1,2,3,4,5,6,7,8];
 let result = [];
-
+let i = 0;
+while (i < array.length) {
+  if (array[i] % 2 !== 0) {
+    result.push(array[i]);
+  }
+  i++;
+}
+console.log(result); // [1,3,5,7]
 
 //export result
 module.exports = result;
